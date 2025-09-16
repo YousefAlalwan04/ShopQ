89e2846ced2e5c32b31217307b38e6a0c52478c1
Yousef Al-Alwan <73909476+YousefAlAlwan04@users.noreply.github.com>
2025-09-16 14:08:44 +0300

    removed a test

--- src/main/java/com/shopQ/MainShopQ/products/repository/ProductRepo.java
+++ src/main/java/com/shopQ/MainShopQ/products/repository/ProductRepo.java
@@ -8,5 +8,4 @@ import java.util.Optional;
 public interface ProductRepo extends JpaRepository<Product, Long> {
     Optional<Product> findByProductName(String productName);
 
-    Optional<Product> findByQuantity(int quantity);
 }
